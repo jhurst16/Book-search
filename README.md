c94f4a91377dbf987d17c0862d32d5ebe656cccf
Jeremy's commits <jeremyhurst16@gmail.com>
2021-10-08 21:46:29 -0400

    app is working correctly

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Book-search
+# Book search
+
+A website to search for books.
+
+## Languages
+
+```
+HTML
+CSS
+JavaScript
+React
+```
+
+## Usage
+
+Type in a book title to see all searches related to that particular search term!
+
+## Links
+
+Github Repo: https://github.com/jhurst16/Book-search
+
+## Screenshots
+
+[Google Book Search - Google Chrome 10_8_2021 9_45_30 PM](https://user-images.githubusercontent.com/79426564/136639510-a66e8def-d104-4f37-b4d2-df1e375772af.png)
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,50 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String!
+    email: String!
+    password: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  type Query {
+    me: User
+  }
+  type Book {
+    authors: [String]
+    description: String
+    bookId: String!
+    title: String
+    image: String
+    link: String
+  }
+  input BookInput {
+    bookId: String!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  type Users {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String, email: String, password: String): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
